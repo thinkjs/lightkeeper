75419f2ada08b248938e6ba2e08ae6f43ac6ef41
lizheming <secretlzm007@gmail.com>
2017-10-30 21:44:05 +0800

    0.0.2

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,13 @@
 const path = require('path');
 const pkgName = 'pharos';
+const production = process.env.npm_lifecycle_event.includes('production');
 module.exports = {
   entry: {
     'main': path.join(__dirname, 'src/main.js')
   },
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: `${pkgName.toLowerCase()}.js`,
+    filename: `${pkgName.toLowerCase()}${production ? '.min' : ''}.js`,
     library: pkgName,
     libraryTarget: 'umd'
   },
