659dee7d0a89afe13a3a046a9785fc6280a902eb
lizheming <i@imnerd.org>
2019-06-16 15:05:00 +0800

    ls 缓存的时候没有 script 标签需要做兼容

--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@ const pharos = new Pharos();
 
 global.addEventListener('load', function() {
   const script = document.querySelector('script[data-siteid][data-host]');
-  pharos.site_id = script.getAttribute('data-siteid');
-  pharos.host = script.getAttribute('data-host');
+  if (script) {
+    pharos.site_id = script.getAttribute('data-siteid');
+    pharos.host = script.getAttribute('data-host');
+  }
 });
 
 module.exports = pharos;
